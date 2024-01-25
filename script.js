412327fe3767c3c129fecb3dd47bc0bc4cdc7747
Ioanna Tag <52848577+Giannoulalala@users.noreply.github.com>
2024-01-25 15:26:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const jokeel = document.getElementById("joke");
+const get_joke = document.getElementById("get_joke");
+
+
+get_joke.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+    const jokeRes = await fetch('https://icanhazdadjoke.com/',
+    {
+        headers: {
+            accept: 'application/json'
+        }
+    });
+
+    const joke = await jokeRes.json();
+
+    console.log(joke);
+    jokeel.innerHTML = joke.joke;
+}
